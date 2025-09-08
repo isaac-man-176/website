0fdeaca6fce5deef3e2b0ff869e10653ca1a2421
Isaac Man <103552680+haydayisawsome@users.noreply.github.com>
2025-09-07 22:05:32 -0400

    fix images loading part 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 export default function About() {
     const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/public/Isaac_Man___General_Resume___Updated_Sept_8__2025";                 // PDF in public folder
-    link.download = "/public/Isaac_Man___General_Resume___Updated_Sept_8__2025.pdf";       // filename when downloaded
+    link.href = "/Isaac_Man___General_Resume___Updated_Sept_8__2025";                 // PDF in public folder
+    link.download = "/Isaac_Man___General_Resume___Updated_Sept_8__2025.pdf";       // filename when downloaded
     link.click();
   }
   return (
@@ -21,10 +21,10 @@ export default function About() {
         If you’re interested in connecting with me or if you have any further inquiries, feel free to reach out through LinkedIn or email!
       </p>
       <div className = "imageSection">
-      <img src="src\images\IsaacManProfilePic.jpeg" alt = "Isaac Man"></img>
+      <img src="\images\IsaacManProfilePic.jpeg" alt = "Isaac Man"></img>
       <div className = "smallButtons">
-      <a href="https://github.com/isaac-man-176" target="_blank" rel="noopener noreferrer"><img src="/src/images/githublogo.png" className = "githubLogo"></img></a>
-      <a href="https://www.linkedin.com/in/isaac-man-865621305/" target="_blank" rel="noopener noreferrer"><img src="/src/images/linkedinlogo.png" className = "linkedinLogo"></img></a>
+      <a href="https://github.com/isaac-man-176" target="_blank" rel="noopener noreferrer"><img src="/images/githublogo.png" className = "githubLogo"></img></a>
+      <a href="https://www.linkedin.com/in/isaac-man-865621305/" target="_blank" rel="noopener noreferrer"><img src="/images/linkedinlogo.png" className = "linkedinLogo"></img></a>
       <button className="resumeButton" onClick={handleDownload}>Resume</button>
       </div>
       </div>
